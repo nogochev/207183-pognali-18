0ae12d743c503f971d7fbc7c94453ad4917a4564
Nogochev Dmitry <webmasterdima@yandex.ru>
2020-01-29 21:21:06 +0300

    правка package.json

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,6 @@ gulp.task("css", function () {
     .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(postcss([autoprefixer()]))
-    .pipe(gulp.dest("build/css"))
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
